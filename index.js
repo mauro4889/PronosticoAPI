1497f6c8d8833ea212abebf7099dac31b26523bc
mauro4889 <mauro4889@gmail.com>
2022-06-16 11:48:45 -0300

    Se agrego peticion a la api y se muestra en el html

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
 const ciudad = document.getElementById('inputCiudad');
 const buscar = document.getElementById('btnBuscar');
-const JSON = './js/city.list.json'
+const templateCard = document.querySelector('#templateCard').content;
+const fragment = document.createDocumentFragment();
+const containerCard = document.querySelector('.container__card');
+const urlIcon = 'http://openweathermap.org/img/wn/'
 
-const mostrarBusqueda = (dataJSON, ciudad) =>{
-    const resultado = dataJSON.find(el =>
-        el.name == ciudad);
-    console.log(resultado);
-}
 
-const buscarCiudad = async (objJSON) =>{
+const buscarCiudad = async () =>{
     try{
         const nombreCiudad = ciudad.value;
-        const api = await fetch(objJSON );
+        const api = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${nombreCiudad}&lang=es&appid=848ba8d0aa279313a89c7fff6b8639d6`);
         const data = await api.json();
-        console.log(nombreCiudad);
-        mostrarBusqueda(data, nombreCiudad)
+        console.log(data);
+        mostrarClima(data);
     }
     catch (error){
         console.log(error);
     }
 }
 
+const mostrarClima = (datosApi) =>{
+    let icono = datosApi.weather[0].icon;
+    let descripcion = datosApi.weather[0].description;
+    let temperatura = datosApi.main.temp;
+    temperatura -= 273.15;
+    temperatura = temperatura.toFixed(2);
+    templateCard.querySelector('h2').textContent = `${ciudad.value}`;
+    templateCard.querySelector('img').setAttribute('src', `${urlIcon}${icono}.png`);
+    templateCard.querySelector('h3').textContent = `${temperatura} C°`;
+    templateCard.querySelector('p').textContent = `${descripcion}`;
+    const clone = document.importNode(templateCard, true);
+    fragment.appendChild(clone);
+    containerCard.appendChild(fragment);
+}
 
-
+const limpiar = () => {
+    while (containerCard.firstChild) {
+        containerCard.removeChild(containerCard.firstChild);
+    }
+}
 
 buscar.addEventListener('click', ()=>{
-    buscarCiudad(JSON);
+    limpiar();
+    buscarCiudad();
 })
